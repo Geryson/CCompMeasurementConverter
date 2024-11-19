157fb6ac4df90e46b175cbf967ca60e4fe87e335
Csávás Gergely <csavas.g95@gmail.com>
2024-11-19 15:49:37 +0100

    Felület definiálása, állapotértékek elkészítése, gombok alapvető működése elkészítve

--- app/src/main/java/com/ggantycc/ccompmeasurementconverter/MainActivity.kt
+++ app/src/main/java/com/ggantycc/ccompmeasurementconverter/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ggantycc.ccompmeasurementconverter.ui.theme.CCompMeasurementConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +24,42 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var text by remember { mutableStateOf("") }
+            var convertedValue by remember { mutableStateOf("-") }
             CCompMeasurementConverterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Column(modifier = Modifier.padding(30.dp)) {
+                    OutlinedTextField(
+                        value = text,
+                        onValueChange = { text = it },
+                        label = { Text("Value") },
+                        modifier = Modifier.fillMaxWidth()
                     )
+                    Row() {
+                        Button(
+                            onClick = {
+                                convertedValue = (text.toDouble() * 0.621371).toString()
+                            }
+                        ) {
+                            Text("km -> miles")
+                        }
+                        Button(
+                            onClick = {
+                                convertedValue = (text.toDouble() * 3.28084).toString()
+                            }
+                        ) {
+                            Text("km -> yard")
+                        }
+                        Button(
+                            onClick = {
+                                convertedValue = (text.toDouble() * 39.3701).toString()
+                            }
+                        ) {
+                            Text("km -> foot")
+                        }
+                    }
+                    Text(convertedValue)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CCompMeasurementConverterTheme {
-        Greeting("Android")
     }
 }
